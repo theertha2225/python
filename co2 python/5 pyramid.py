1dba887c5214eeedfea40064d75de72776169638
Manukrishna2000 <96103298+Manukrishna2000@users.noreply.github.com>
2022-03-03 10:44:23 +0530

    Delete co2 python directory

--- co2 python/5 pyramid.py
+++
@@ -1,6 +0,0 @@
-n=int(input("enter range"))
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(i+j,end=" ")
-    print(" ")
-        
--- co2 python/7 ing.py
+++
@@ -1,9 +0,0 @@
-def ing():
-    str=input("enter a string")
-    last=str[-3:]
-    if(last=="ing"):
-        str+="ly"
-    else:
-        str+="ing"
-    print(str)
-ing()
