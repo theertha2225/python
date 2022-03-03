ed8098014e8f68eaef6427157568d81d25bdc3f2
fidhamariyam12 <44356748+fidhamariyam12@users.noreply.github.com>
2022-03-02 22:36:14 -0800

    Add files via upload

---
+++ S1_22/CO1/17.py
@@ -0,0 +1,6 @@
+d={4:"fidha",1:"insha",2:"aysha",3:"aslaha"}
+print(d)
+newd=sorted(d.items())
+print(newd)
+newd=dict(sorted(d.items(),reverse=True))
+print(newd)
---
+++ S1_22/CO4/2.py
@@ -0,0 +1,25 @@
+class bankaccount:
+    def __init__(self):
+        accountnumber=float(input("enter account number"))
+        accountname=str(input("enter acoount name"))
+        accounttype=str(input("type of account"))
+        self.balance=0
+        print("------------------------")
+    def deposit(self):
+        amount=float(input("enter amount to deposit"))
+        self.balance+=amount
+        print("amount deposited is",amount)
+        print("------------------------")
+    def withdraw(self):
+        amount=float(input("enter amount to witdraw"))
+        self.balance-=amount
+        print("amount withdrawn is",amount)
+        print("------------------------")
+    def display(self):
+        print("avilable balance:",self.balance)
+        print("------------------------")
+
+s=bankaccount()
+s.deposit()
+s.withdraw()
+s.display()
