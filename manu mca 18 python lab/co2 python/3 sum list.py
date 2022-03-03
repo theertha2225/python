08335b5883e20565367f0918919e1b90a0392a5c
Manukrishna2000 <96103298+Manukrishna2000@users.noreply.github.com>
2022-03-03 12:07:21 +0530

    Delete manu mca 18 python lab directory

--- manu mca 18 python lab/co2 python/3 sum list.py
+++
@@ -1,6 +0,0 @@
-def s():
-    l=[1,6,5,2,8]
-    print(l)
-    a=(sum(l))
-    print(a)
-s()
--- manu mca 18 python lab/co2 python/8 longest.py
+++
@@ -1,6 +0,0 @@
-l=[]
-n=int(input("how many words entered"))
-print("enter word")
-for i in range(n):
-    l.append(len(input()))
-print("the longest word with size----"+str(max(l)))
